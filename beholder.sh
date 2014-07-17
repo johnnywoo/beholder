11c6f53bea836035bdc45343fbea9fef234fb1e6
Aleksandr Galkin <agalkin@agalkin.ru>
2014-07-17 19:38:17 +0400

    Added a description into beholder.sh

--- beholder.sh
+++ beholder.sh
@@ -6,6 +6,7 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 jar_file="$DIR/build/libs/beholder.jar"
 
+# only build the gradle project if jar is older than the sources
 if [ -e "$jar_file" ]; then
     newest_source_file_mtime="$(find "$DIR" -type f -path '*/src/*' -printf '%T@ %p\n' | sort -n | tail -1 | cut -f1 -d.)"
     jar_file_mtime="$(find "$DIR" -path "$jar_file" -printf '%T@ %p\n' | sort -n | tail -1 | cut -f1 -d.)"

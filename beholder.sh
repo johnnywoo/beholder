92afe00e6757a069a43df0f92bd42c2ea51804d8
Aleksandr Galkin <agalkin@agalkin.ru>
2014-07-17 19:36:39 +0400

    Checking if sources are older than the jar in beholder.sh

--- beholder.sh
+++ beholder.sh
@@ -2,6 +2,20 @@
 
 set -e
 
-./gradlew
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-java -jar build/libs/beholder.jar "$@"
+jar_file="$DIR/build/libs/beholder.jar"
+
+if [ -e "$jar_file" ]; then
+    newest_source_file_mtime="$(find "$DIR" -type f -path '*/src/*' -printf '%T@ %p\n' | sort -n | tail -1 | cut -f1 -d.)"
+    jar_file_mtime="$(find "$DIR" -path "$jar_file" -printf '%T@ %p\n' | sort -n | tail -1 | cut -f1 -d.)"
+
+    if [ "$newest_source_file_mtime" -gt "$jar_file_mtime" ]; then
+        ./gradlew
+    fi
+else
+    # there is no jar yet
+    ./gradlew
+fi
+
+java -jar "$jar_file" "$@"

ce9f12848bc3cb1796cd85614923ec6d335008ad
Aleksandr Galkin <agalkin@agalkin.ru>
2018-04-27 18:24:58 +0300

    `parse syslog` now recognizes a subset of IETF syslog format

--- src/main/kotlin/ru/agalkin/beholder/inflaters/SyslogInflater.kt
+++ src/main/kotlin/ru/agalkin/beholder/inflaters/SyslogInflater.kt
@@ -28,7 +28,7 @@ class SyslogInflater : Inflater {
         val payload = message.getPayload()
         val matcher = syslogNginxRegex.matcher(payload)
         if (!matcher.find()) {
-            return false
+            return parseIetfSyslog(message)
         }
 
         val priority = matcher.group("priority")
@@ -50,6 +50,68 @@ class SyslogInflater : Inflater {
         val tag = matcher.group("tag")
         if (tag != null) {
             message["program"] = tag
+        }
+
+        val headerLength = matcher.end()
+        message["payload"] = payload.substring(headerLength)
+
+        return true
+    }
+
+    // <15>1 2018-04-27T17:49:03+03:00 hostname program 73938 - - Message
+    private val syslogIetfRegex = Pattern.compile(
+        """
+            ^
+            < (?<priority>[0-9]+) >1
+            \s (?<date> \d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d (?: Z | [+-]\d\d:\d\d) )
+            \s (?<host> [^\s]+)
+            \s (?<program> [^\s]+)
+            \s (?<pid> [^\s]+)
+            \s (?<messageId> [^\s]+)
+            \s [^\s]+
+            \s
+        """,
+        Pattern.COMMENTS
+    )
+
+    private fun parseIetfSyslog(message: Message): Boolean {
+        // попробуем формат IETF-сислога
+        // пока что поддерживается не всё: structured data должна быть - или хотя бы не содержать пробелов
+        val payload = message.getPayload()
+        val matcher = syslogIetfRegex.matcher(payload)
+        if (!matcher.find()) {
+            return false
+        }
+
+        val priority = matcher.group("priority")
+        if (priority != null) {
+            try {
+                val int = Integer.parseInt(priority)
+                // The Priority value is calculated by first multiplying the Facility number by 8
+                // and then adding the numerical value of the Severity.
+                message["facility"] = (int / 8).toString()
+                message["severity"] = (int % 8).toString()
+            } catch (ignored: NumberFormatException) {}
+        }
+
+        val date = matcher.group("date")
+        if (date != null) {
+            message["date"] = date.replace(Regex("Z$"), "+00:00")
+        }
+
+        val host = matcher.group("host")
+        if (host != null) {
+            message["host"] = host
+        }
+
+        val tag = matcher.group("program")
+        if (tag != null) {
+            message["program"] = tag
+        }
+
+        val pid = matcher.group("pid")
+        if (pid != null) {
+            message["pid"] = pid
         }
 
         val headerLength = matcher.end()

69ff91b9063193779c956c54856b199db80cd28a
Aleksandr Galkin <agalkin@agalkin.ru>
2018-08-10 18:00:37 +0300

    NPE on `set $field env NOT_IN_ENV`

--- src/main/kotlin/ru/agalkin/beholder/formatters/EnvFormatter.kt
+++ src/main/kotlin/ru/agalkin/beholder/formatters/EnvFormatter.kt
@@ -4,7 +4,7 @@ import ru.agalkin.beholder.FieldValue
 import ru.agalkin.beholder.Message
 
 class EnvFormatter(envVarName: String) : Formatter {
-    private val envVarValue = FieldValue.fromString(System.getenv(envVarName))
+    private val envVarValue = FieldValue.fromString(System.getenv(envVarName) ?: "")
 
     override fun formatMessage(message: Message)
         = envVarValue

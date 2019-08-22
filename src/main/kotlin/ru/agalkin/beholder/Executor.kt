9bbb776e605a2346d10f1c45a86a0893cff2a6b6
Aleksandr Galkin <g@dostavista.ru>
2019-08-22 11:50:16 +0300

    Preventing fatal exception in Executor under high load

--- src/main/kotlin/ru/agalkin/beholder/Executor.kt
+++ src/main/kotlin/ru/agalkin/beholder/Executor.kt
@@ -11,6 +11,12 @@ class Executor {
         60, TimeUnit.SECONDS,
         SynchronousQueue()
     )
+    init {
+        // Если в пуле все треды заняты, задачу выполнит тот тред, который пихает её в очередь.
+        // По умолчанию в такой ситуации всё падает (RejectedExecutionException),
+        // а так мы просто притормозим на пиковой нагрузке.
+        pool.rejectedExecutionHandler = ThreadPoolExecutor.CallerRunsPolicy()
+    }
 
     fun execute(block: () -> Unit) {
         pool.execute(block)
